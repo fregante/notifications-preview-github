fregante/notifications-preview-github

commit a6567b26d8906f6c6ea554b0202d03534329a629

Simplify border/margins

style.css
@@ -1,5 +1,8 @@
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu {
     width: 750px;
+    border: none;
+    padding: 2px 2px 3px;
+    overflow: hidden;
 }
 
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu .dropdown-item:hover {
@@ -9,8 +12,7 @@
 }
 
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu #my-github-notification-list {
-    padding: 2px 0;
-    max-height: 50%
+    padding: 0;
 }
 
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu {
@@ -18,10 +20,9 @@
 }
 
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu .paginate-container {
-    margin: 0px;
     display: none;
 }
 
 .user-nav a.notification-indicator~.dropdown-menu-content .dropdown-menu .boxed-group {
-    margin-bottom: 20px;
+    margin-bottom: 0;
 }

commit dc4fc9f0dafa7620b88c5df45ff57cd7cc9e612f

Fix issues caused by a6567b2

index.js
@@ -70,6 +70,7 @@
 					$('#NPG-item .loading-notification').remove();
 					$('#NPG').show();
 					notificationList.find('.paginate-container').remove();
+					notificationList.find('.tooltipped-s').removeClass('tooltipped-s').addClass('tooltipped-n');
 					if (notificationList[0].scrollHeight === notificationList[0].offsetHeight) {
 						notificationList.css("overflow-y", "hidden");
 					} else {

style.css
@@ -17,5 +17,9 @@
 }
 
 #NPG-item .boxed-group {
+    margin-bottom: 20px;
+}
+
+#NDE-item .boxed-group:last-child {
     margin-bottom: 0;
 }